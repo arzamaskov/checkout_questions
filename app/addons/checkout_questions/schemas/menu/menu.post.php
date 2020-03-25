6851073a8ba2064cb6de7f99bcd0bb0eb742d717
Andrey Arzamaskov <arzamaskov@gmail.com>
2020-03-25 12:08:48 +0400

    UNIT-7.2 : Create schema menu

---
+++ app/addons/checkout_questions/schemas/menu/menu.post.php
@@ -0,0 +1,11 @@
+<?php
+
+$schema['central']['website']['items']['checkout_questions'] = array(
+    'attrs' => array(
+        'class'=>'is-addon'
+    ),
+    'href' => 'checkout_questions.manage',
+    'position' => 500
+);
+
+return $schema;
