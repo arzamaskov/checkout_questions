2f7769b0a3258154c66b4d9530d2934d98f09477
Andrey Arzamaskov <arzamaskov@gmail.com>
2020-03-25 12:28:32 +0400

    UNIT-7.2 : Modify addon backend controller

--- app/addons/checkout_questions/controllers/backend/checkout_questions.php
+++ app/addons/checkout_questions/controllers/backend/checkout_questions.php
@@ -5,7 +5,11 @@ use Tygh\Registry;
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
 if ($mode == 'manage') {
-    $data = 2;
 
-    Tygh::$app['view']->assign('all_data', $data);
+    list($checkout_questions, $params) = fn_get_checkout_questions($_REQUEST, DESCR_SL, Registry::get('settings.Appearance.admin_elements_per_page'));
+
+    Tygh::$app['view']->assign(array(
+        'checkout_questions'  => $checkout_questions,
+        'search' => $params,
+    ));
 }
