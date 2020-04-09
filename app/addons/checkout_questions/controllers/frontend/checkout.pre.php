f3bc0a904c1d53b3441d26e53cfb403b8ed852ec
Andrey Arzamaskov <arzamaskov@gmail.com>
2020-04-09 16:20:49 +0400

    UNIT-7.2 : Modify PRE checkout controller

--- app/addons/checkout_questions/controllers/frontend/checkout.pre.php
+++ app/addons/checkout_questions/controllers/frontend/checkout.pre.php
@@ -8,7 +8,13 @@ if ($mode == 'place_order') {
     if(isset($cart['checkout_questions_data'])) {
         unset($cart['checkout_questions_data']);
     }
-    $checkout_questions_data = $_REQUEST['checkout_questions_data'];
-        Tygh::$app['session']['cart']['checkout_questions_data'][] = $checkout_questions_data;
+    $data = $_REQUEST['checkout_questions_data'];
+    $checkout_questions_data = array();
 
+    foreach ($data as $key => $value) {
+        $checkout_questions_data[$key]['title'] = db_get_field("SELECT title FROM ?:checkout_question_descriptions WHERE question_id = ?i", $key);
+        $checkout_questions_data[$key]['value'] = $value;
+    }
+
+    Tygh::$app['session']['cart']['checkout_questions_data'][] = $checkout_questions_data;
 }
